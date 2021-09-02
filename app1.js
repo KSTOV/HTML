559f16b43aba7e0c4accce5e19d037f01ffbe440
KLan Stovall <kland13@gmail.com>
2021-09-02 12:36:05 -0700

    refactored javascript into functions

--- app1.js
+++ app1.js
@@ -1,11 +1,37 @@
-let userAnswer = prompt('Are you coding? Yes or No');
-
-if (userAnswer.toLocaleLowerCase() == 'yes')
+function userQuestion()
 {
-    let url = "https://c.tenor.com/GfSX-u7VGM4AAAAM/coding.gif"
-    document.write('<img src="' + url + '">');
+    let userAnswer = prompt('Are you coding? Yes or No');
+    
+    if (userAnswer.toLocaleLowerCase() == 'yes')
+    {
+        let url = "https://c.tenor.com/GfSX-u7VGM4AAAAM/coding.gif"
+        document.write('<img src="' + url + '">');
+    }
+    else
+    {
+        window.location = "https://kstov.github.io/HTML/blank";
+    }
 }
-else
+
+function getUserName()
 {
-    window.location = "https://kstov.github.io/HTML/blank";
+    let userName = prompt('Please Enter Name: ');
+    
+    document.write('Welcome ' + userName);
+}
+
+//code from https://www.javatpoint.com/random-image-generator-in-javascript
+function getRandomImage()
+{
+    var randomImage = new Array();
+
+    randomImage[0] = "https://media4.giphy.com/media/l3vRfNA1p0rvhMSvS/giphy.gif";
+    randomImage[1] = "https://c.tenor.com/2gfRHLv6GZ8AAAAd/code-coding.gif";
+    randomImage[2] = "https://i.gifer.com/Ry6p.gif";
+    randomImage[3] = "https://media.giphy.com/media/USV0ym3bVWQJJmNu3N/giphy.gif";
+    randomImage[4] = "https://media.giphy.com/media/lGD7VxrtwNalqhx68w/giphy.gif";
+
+    var number = Math.floor(Math.random()*randomImage.length)
+
+    document.write('<img src="'+randomImage[number]+'" />');
 }

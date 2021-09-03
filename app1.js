4c36d464f25acc8724f08a5d2a4842aff302458b
KLan Stovall <kland13@gmail.com>
2021-09-02 17:05:09 -0700

    loops

--- app1.js
+++ app1.js
@@ -1,15 +1,20 @@
 function userQuestion()
 {
-    let userAnswer = prompt('Are you coding? Yes or No');
-    
-    if (userAnswer.toLocaleLowerCase() == 'yes')
+    let userAnswer = prompt('Having a nice day? Yes or No');
+    while(userAnswer.toLowerCase() == 'yes' || 'no')
     {
-        let url = "https://c.tenor.com/GfSX-u7VGM4AAAAM/coding.gif"
-        document.write('<img src="' + url + '">');
-    }
-    else
-    {
-        window.location = "https://kstov.github.io/HTML/blank";
+        let userAnswer = prompt('Are you enjoying coding? Yes or No');
+            
+        if (userAnswer.toLowerCase() == 'yes')
+        {
+            let url = "https://c.tenor.com/GfSX-u7VGM4AAAAM/coding.gif"
+            document.write('<img src="' + url + '">');
+            break;
+        }
+        else if (userAnswer == 'no')
+        {
+            alert('Come on you know you are!')
+        }
     }
 }
 
@@ -25,13 +30,31 @@ function getRandomImage()
 {
     var randomImage = new Array();
 
-    randomImage[0] = "https://media4.giphy.com/media/l3vRfNA1p0rvhMSvS/giphy.gif";
-    randomImage[1] = "https://c.tenor.com/2gfRHLv6GZ8AAAAd/code-coding.gif";
-    randomImage[2] = "https://i.gifer.com/Ry6p.gif";
-    randomImage[3] = "https://media.giphy.com/media/USV0ym3bVWQJJmNu3N/giphy.gif";
-    randomImage[4] = "https://media.giphy.com/media/lGD7VxrtwNalqhx68w/giphy.gif";
+    randomImage[0] = ""
+    randomImage[1] = "https://media4.giphy.com/media/l3vRfNA1p0rvhMSvS/giphy.gif";
+    randomImage[2] = "https://c.tenor.com/2gfRHLv6GZ8AAAAd/code-coding.gif";
+    randomImage[3] = "https://i.gifer.com/Ry6p.gif";
+    randomImage[4] = "https://media.giphy.com/media/USV0ym3bVWQJJmNu3N/giphy.gif";
+    randomImage[5] = "https://media.giphy.com/media/lGD7VxrtwNalqhx68w/giphy.gif";
 
-    var number = Math.floor(Math.random()*randomImage.length)
+    // var number = Math.floor(Math.random()*randomImage.length)
 
-    document.write('<img src="'+randomImage[number]+'" />');
+    
+    for (let i=0, len = randomImage.length; i < len; ++i)
+    {
+        pickNumber = prompt('Pick a number from 1 to 5');
+        if(pickNumber > 5)
+        {
+            alert('Try Again! Pick a number from 1 to 5')
+        }
+        else if(pickNumber < 1)
+        {
+            alert('Try Again! Pick a number from 1 to 5')
+        }
+        else
+        {
+            document.write('<img src="'+randomImage[pickNumber]+'" />');
+            break;
+        }
+    }
 }
